19146992e01e29174e224442e4d04db87c46f7d4
Joao Figueiredo <joaonvfigueiredo@gmail.com>
2015-07-22 17:31:32 +0100

    refactored tests and fixed pep8 warnings

--- tests/ngsi_tests.py
+++ tests/ngsi_tests.py
@@ -7,52 +7,70 @@ from ngsi import Client
 class NgsiClientTests(TestCase):
     def setUp(self):
         self.test_host = '130.206.117.120'
+        self.test_elements = [
+            {
+                "type": "Room",
+                "isPattern": "false",
+                "id": "Room3",
+                "attributes": [
+                    {
+                        "name": "temperature",
+                        "type": "float",
+                        "value": "23"
+                    },
+                    {
+                        "name": "pressure",
+                        "type": "integer",
+                        "value": "720"
+                    }
+                ]
+            },
+            {
+                "type": "Room",
+                "isPattern": "false",
+                "id": "Room4",
+                "attributes": [
+                    {
+                        "name": "temperature",
+                        "type": "float",
+                        "value": "33"
+                    },
+                    {
+                        "name": "pressure",
+                        "type": "integer",
+                        "value": "1100"
+                    }
+                ]
+            }
+        ]
+        self.test_entities = [
+            {
+                "type": "Room",
+                "isPattern": "false",
+                "id": "Room1"
+            }
+        ]
+        self.test_reference = 'http://requestb.in/1g22in51'
+        self.test_attributes = ["temperature"]
+        self.test_duration = timedelta(days=30)
+        self.test_notify_conditions = [
+            {
+                "type": "ONTIMEINTERVAL",
+                "condValues": [
+                    "PT10S"
+                ]
+            }
+        ]
 
     def test_version(self):
         c = Client(host=self.test_host)
         self.assertEqual(c.version()['version'], '0.19.0')
 
     def test_create_context(self):
-        test_elements = [
-        {
-            "type": "Room",
-            "isPattern": "false",
-            "id": "Room3",
-            "attributes": [
-                {
-                "name": "temperature",
-                "type": "float",
-                "value": "23"
-                },
-                {
-                "name": "pressure",
-                "type": "integer",
-                "value": "720"
-                }
-            ]
-        },
-        {
-            "type": "Room",
-            "isPattern": "false",
-            "id": "Room4",
-            "attributes": [
-                {
-                "name": "temperature",
-                "type": "float",
-                "value": "33"
-                },
-                {
-                "name": "pressure",
-                "type": "integer",
-                "value": "1100"
-                }
-            ]
-        }]
-
         c = Client(host=self.test_host)
-        r = c.create_context(test_elements)
+        r = c.create_context(self.test_elements)
         for element in r:
-            self.assertEqual(element['statusCode']['code'],'200')
+            self.assertEqual(element['statusCode']['code'], '200')
 
     def test_get_context(self):
         test_entities = [
@@ -62,62 +80,24 @@ class NgsiClientTests(TestCase):
                 "id": "Room*"
             }
         ]
-
         c = Client(host=self.test_host)
         r = c.get_context(test_entities)
         for element in r:
-            self.assertEqual(element['statusCode']['code'],'200')
+            self.assertEqual(element['statusCode']['code'], '200')
 
     def test_subscribe_context(self):
-        test_reference = 'http://requestb.in/1g22in51'
-        test_entities = [
-            {
-                "type": "Room",
-                "isPattern": "false",
-                "id": "Room1"
-            }
-        ]
-        test_attributes = ["temperature"]
-        test_duration = timedelta(days=30)
-        test_notify_conditions = [
-            {
-                "type": "ONTIMEINTERVAL",
-                "condValues": [
-                    "PT10S"
-                ]
-            }
-        ]
-
         c = Client(host=self.test_host)
         r = c.subscribe_context(
-            entities=test_entities,
-            callback_url=test_reference,
-            duration=test_duration,
-            notification_type=test_notify_conditions,
-            attributes=test_attributes
+            entities=self.test_entities,
+            callback_url=self.test_reference,
+            duration=self.test_duration,
+            notification_type=self.test_notify_conditions,
+            attributes=self.test_attributes
         )
 
         self.assertTrue('subscriptionId' in r)
 
     def test_update_subscribe_context(self):
-        test_reference = 'http://requestb.in/1g22in51'
-        test_entities = [
-            {
-                "type": "Room",
-                "isPattern": "false",
-                "id": "Room1"
-            }
-        ]
-        test_attributes = ["temperature"]
-        test_duration = timedelta(days=30)
-        test_notify_conditions = [
-            {
-                "type": "ONTIMEINTERVAL",
-                "condValues": [
-                    "PT10S"
-                ]
-            }
-        ]
         test_update_notify_conditions = [
             {
                 "type": "ONTIMEINTERVAL",
@@ -129,11 +109,11 @@ class NgsiClientTests(TestCase):
 
         c = Client(host=self.test_host)
         r = c.subscribe_context(
-            entities=test_entities,
-            callback_url=test_reference,
-            duration=test_duration,
-            notification_type=test_notify_conditions,
-            attributes=test_attributes
+            entities=self.test_entities,
+            callback_url=self.test_reference,
+            duration=self.test_duration,
+            notification_type=self.test_notify_conditions,
+            attributes=self.test_attributes
         )
         sub_id = r['subscriptionId']
 
@@ -141,40 +121,21 @@ class NgsiClientTests(TestCase):
             sub_id,
             notifyConditions=test_update_notify_conditions
         )
-        self.assertEqual(r['subscriptionId'],sub_id)
+        self.assertEqual(r['subscriptionId'], sub_id)
 
         r = c.unsubscribe_context(sub_id)
 
     def test_unsubscribe_context(self):
-        test_reference = 'http://requestb.in/1g22in51'
-        test_entities = [
-            {
-                "type": "Room",
-                "isPattern": "false",
-                "id": "Room1"
-            }
-        ]
-        test_attributes = ["temperature"]
-        test_duration = timedelta(days=30)
-        test_notify_conditions = [
-            {
-                "type": "ONTIMEINTERVAL",
-                "condValues": [
-                    "PT10S"
-                ]
-            }
-        ]
-
         c = Client(host=self.test_host)
         r = c.subscribe_context(
-            entities=test_entities,
-            callback_url=test_reference,
-            duration=test_duration,
-            notification_type=test_notify_conditions,
-            attributes=test_attributes
+            entities=self.test_entities,
+            callback_url=self.test_reference,
+            duration=self.test_duration,
+            notification_type=self.test_notify_conditions,
+            attributes=self.test_attributes
         )
         sub_id = r['subscriptionId']
 
         r = c.unsubscribe_context(sub_id)
 
-        self.assertEqual(r['statusCode']['code'],'200')
+        self.assertEqual(r['statusCode']['code'], '200')
